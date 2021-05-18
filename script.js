80cfe4821490bc9178dedad64ce98ba7272e0f28
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-05-18 14:27:01 +0530

    removed comments

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ menu.addEventListener("click",()=>
     menu.innerHTML = '<img src="https://img.icons8.com/ios-filled/30/000000/menu--v3.png"/>'}
 } )
 
+function resize() {
+    // console.log("height: ", window.innerHeight, "px");
+    // console.log("width: ", window.innerWidth, "px");
+    if(window.innerWidth >=700)
+    {
+        sidebarShow = true;
+        sidebar.style.display="block";
+    menu.innerHTML = '<img src="https://img.icons8.com/ios-glyphs/50/000000/macos-close.png"/>'}
+  }
+  
+  window.onresize = resize;
